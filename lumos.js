ddf8bbf3559bf32d0915035eaa6b8136e8d5bc3d
Cam Pedersen <diffference@gmail.com>
2012-01-12 19:59:13 -0500

    [minor] make sure there is always a color

--- lumos.js
+++ lumos.js
@@ -3,7 +3,7 @@
 setInterval(function(){},1000); //keepalive
 var colors = require('colors');
 var size = process.stdout.getWindowSize();
-var color = process.argv.pop() || 'white';
+var color = process.argv.length > 2 ? process.argv.pop() : 'white';
 
 for (var i = 0; i < size[1]; i++) {
   process.stdout.write(Array(size[0] + 1).join(' ')[color].inverse);
